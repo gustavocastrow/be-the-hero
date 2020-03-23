a8794318b0aa69cc6c2e2d396217d26d344eba61
gustavocastrow <gustavocastrocs@gmail.com>
2020-03-23 17:00:40 -0300

    Installing Dependencies

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  return res.json( {
+    evento: 'Semana OmniStack 11.0',
+    aluno: 'Gustavo Castro',
+  });
+
+
+})
+
+app.listen(3333);
+
+
